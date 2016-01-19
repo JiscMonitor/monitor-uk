d68752d546965b1324181231a1ca063a17019d0e
Richard <Richard Jones>
2016-01-19 09:13:06 +0000

    prep app for development

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'YOURAPP',
+    name = 'Monitor UK',
     version = '1.0.0',
     packages = find_packages(),
     install_requires = [
@@ -12,12 +12,9 @@ setup(
     url = 'http://cottagelabs.com/',
     author = 'Cottage Labs',
     author_email = 'us@cottagelabs.com',
-    description = 'DESCRIBE YOUR APP',
+    description = 'Monitor UK aggregation and reporting interface',
     classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: Copyheart',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules'
