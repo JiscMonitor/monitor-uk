b91ab73d62d3a9b770bf96566ee6913ddeca8478
Steven Eardley <steve@eardley.xyz>
2016-05-12 13:30:57 +0100

    Added visible change for CI test and fixed path to requirements

--- deploy/ci.sh
+++ deploy/ci.sh
@@ -5,7 +5,7 @@ git submodule update --init --recursive
 
 source ../../bin/activate
 
-pip install -r requirements.txt
+pip install -r ../requirements.txt
 
 sudo supervisorctl reread
 sudo supervisorctl update monitor-uk
